826aa8d06d7917cb20b44d6f47edd9302360eae9
meghan87 <115980448+meghan87@users.noreply.github.com>
2022-10-19 13:46:51 +0530

    added nav bar

--- starter/js/app.js
+++ starter/js/app.js
@@ -1,19 +1,74 @@
-const getElement = (selector) => {
-  const element = document.querySelector(selector)
-
-  if (element) return element
+const getElement = (selector)=>{
+  const element = document.querySelector(selector);
+  if(element) return element
   throw Error(
-    `Please double check your class names, there is no ${selector} class`
+    `${selector} no element found with this name`
   )
 }
 
-const links = getElement('.nav-links')
-const navBtnDOM = getElement('.nav-btn')
-
-navBtnDOM.addEventListener('click', () => {
-  links.classList.toggle('show-links')
+let toggleBtnDOM = getElement('.nav-btn');
+let navLinks = getElement('.nav-links');
+toggleBtnDOM.addEventListener('click',()=>{
+   navLinks.classList.toggle('show-links');
 })
 
-const date = getElement('#date')
-const currentYear = new Date().getFullYear()
-date.textContent = currentYear
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const getElement = (selector) => {
+//   const element = document.querySelector(selector)
+
+//   if (element) return element
+//   throw Error(
+//     `Please double check your class names, there is no ${selector} class`
+//   )
+// }
+
+// const links = getElement('.nav-lks')
+// const navBtnDOM = getElement('.nav-btn')
+
+// navBtnDOM.addEventListener('click', () => {
+//   links.classList.toggle('show-links')
+// })
+
+// const date = getElement('#date')
+// const currentYear = new Date().getFullYear()
+// date.textContent = currentYear
